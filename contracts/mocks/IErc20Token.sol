aebb33a7859c822f794c79855bba3f6c673d9d78
Jack <jackz163299@gmail.com>
2022-04-29 09:47:33 -0400

    remove unused files

--- contracts/mocks/IErc20Token.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.8.0;
-
-interface IErc20Token {
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-}
